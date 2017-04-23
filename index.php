c3e39b4095bb22f74f85a32e41591bb1bf2e5037
Michael Plunkett <michael.lee.plunkett@gmail.com>
2017-04-24 07:07:25 +0900

    Updated HTML

--- index.php
+++ index.php
@@ -1,75 +1,68 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<?php include("/var/www/html/includes/head.html"); ?>
-		<title>Something in the Drain</title>
-	</head>
-	<body>		
-		<div class="wrapper">
-			<?php include("/var/www/html/includes/header.html");  ?>
-			
-			<div class="subbanner">
-				<div class="container">
-				 <h1>Something in the Drain</h1>
-				 <h3>A text-based RPG made using JavaScript</h3>
-				</div>
-			</div>
-			<div class="container">
-				<div class="row">
-					<div class="col-md-12">	
-						<div class="bash-box">
-							<div class="box-top">
-								<div class="box-title">
-									<p>Something in the Drain</p>
-								</div>
-								<div class="box-buttons">
-									<p>X</p>
-								</div>
-							</div>
-							<div class="fight-log" id="fight-log">
-								<p id="game"></p>
-							</div>
-						</div>
-						
-						<div id="startGame">
-							<button type="button"
-							class="button dragButtons" onclick="play();startCheck();">
-							Start</button>
+<head>
 
-							<button type="button"
-							class="button dragButtons" onclick="skipIntro();lifeCheck()">
-							Skip Intro</button>
+</head>
+<body>			
+	<div class="subbanner">
+		<div class="container">
+		 <h1>Something in the Drain</h1>
+		 <h3>A text-based RPG made using JavaScript</h3>
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12">	
+				<div class="bash-box">
+					<div class="box-top">
+						<div class="box-title">
+							<p>Something in the Drain</p>
 						</div>
-						
-						<table class="table_dragonSlayer" id="table_creatureFight" style="display:none">
-							<tr class="head">
-								<td>Your Health</td>
-								<td>Creature Health</td>
-							</tr>
-							<tr class="body">
-								<td id="yourHealth">10</td>
-								<td id="creatureHealth">10</td>
-							</tr>
-						</table>
-						
-						<div id="buttons" style="display:none">
-							<button type="button"
-							class="button dragButtons" onclick="fight();lifeCheck()">
-							Fight the thing</button>
-
-							<button type="button"
-							class="button dragButtons" onclick="runAway();lifeCheck()">
-							Run away</button>
+						<div class="box-buttons">
+							<p>X</p>
 						</div>
 					</div>
+					<div class="fight-log" id="fight-log">
+						<p id="game"></p>
+					</div>
+				</div>
+
+				<div id="startGame">
+					<button type="button"
+					class="button dragButtons" onclick="play();startCheck();">
+					Start</button>
+
+					<button type="button"
+					class="button dragButtons" onclick="skipIntro();lifeCheck()">
+					Skip Intro</button>
+				</div>
+
+				<table class="table_dragonSlayer" id="table_creatureFight" style="display:none">
+					<tr class="head">
+						<td>Your Health</td>
+						<td>Creature Health</td>
+					</tr>
+					<tr class="body">
+						<td id="yourHealth">10</td>
+						<td id="creatureHealth">10</td>
+					</tr>
+				</table>
+
+				<div id="buttons" style="display:none">
+					<button type="button"
+					class="button dragButtons" onclick="fight();lifeCheck()">
+					Fight the thing</button>
+
+					<button type="button"
+					class="button dragButtons" onclick="runAway();lifeCheck()">
+					Run away</button>
 				</div>
 			</div>
-			<div class="push"></div>
 		</div>
-		<?php include("/var/www/html/includes/footer.html"); ?>
+	</div>
+
+	<script src="scripts/setup.js"></script>
+	<script src="scripts/creatureFight.js"></script>
 		
-		<script src="scripts/setup.js"></script>
-		<script src="scripts/creatureFight.js"></script>
-		
-	</body>
+</body>
 </html>
